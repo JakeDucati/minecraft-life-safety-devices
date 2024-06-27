e8f78545bbed5250b58ddf5cab5dd4fded7d4e36
Jake Ducati <107895867+JakeDucati@users.noreply.github.com>
2024-06-27 09:16:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# minecraft-life-safety-devices
+ Fabric mod that adds various life safety devices in buildings to Minecraft such as exit signs, fire alarm devices, etc.
---
+++ src/client/java/com/lifesafetydevices/LifeSafetyDevicesClient.java
@@ -0,0 +1,10 @@
+package com.lifesafetydevices;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LifeSafetyDevicesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
