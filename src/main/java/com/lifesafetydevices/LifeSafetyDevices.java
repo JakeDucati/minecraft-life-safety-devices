fcfb6ca2d6605b4bcfc0d1d7a3a42fa249017bcd
Jake Ducati <107895867+JakeDucati@users.noreply.github.com>
2024-06-27 10:34:39 -0700

    Added White Exit Sign and made it directional

--- src/main/java/com/lifesafetydevices/LifeSafetyDevices.java
+++ src/main/java/com/lifesafetydevices/LifeSafetyDevices.java
@@ -9,7 +9,6 @@ import net.minecraft.util.Identifier;
 
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,26 +17,8 @@ import org.slf4j.LoggerFactory;
 public class LifeSafetyDevices implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("life-safety-devices");
 
-
-    /* Declare and initialize our custom block instance.
-       We set our block material to `METAL`.
-       
-       `strength` sets both the hardness and the resistance of a block to the same value.
-       Hardness determines how long the block takes to break, and resistance determines how strong the block is against blast damage (e.g. explosions).
-       Stone has a hardness of 1.5f and a resistance of 6.0f, while Obsidian has a hardness of 50.0f and a resistance of 1200.0f.
-       
-       You can find the stats of all vanilla blocks in the class `Blocks`, where you can also reference other blocks.
-    */
-    // public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f)); // fabric api version <= 0.77.0
-    // public static final Block EXAMPLE_BLOCK  = new Block(FabricBlockSettings.create().strength(4.0f));
-
-    public static final Block WHITE_EXIT_SIGN = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+    public static final Block WHITE_EXIT_SIGN = new WhiteExitSignBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
     public static final Block TEST = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
-
-    // private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier("tutorial", "test_group"))
-	//         .icon(() -> new ItemStack(WHITE_EXIT_SIGN))
-	//         .build();
-
 
     @Override
     public void onInitialize() {
@@ -47,13 +28,8 @@ public class LifeSafetyDevices implements ModInitializer {
         Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "white_exit_sign"), WHITE_EXIT_SIGN);
         Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "white_exit_sign"), new BlockItem(WHITE_EXIT_SIGN, new FabricItemSettings()));
 
-
-        // test block test texture thing test
+        // test
         Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "test"), TEST);
         Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "test"), new BlockItem(TEST, new FabricItemSettings()));
-
-        // ItemGroupEvents.modifyEntriesEvent(ITEM_GROUP).register(content -> {
-        //     content.add(CUSTOM_ITEM);
-        // });
     }
 }
---
+++ src/main/java/com/lifesafetydevices/WhiteExitSignBlock.java
@@ -0,0 +1,40 @@
+package com.lifesafetydevices;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.Direction;
+
+public class WhiteExitSignBlock extends HorizontalFacingBlock {
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+
+    public WhiteExitSignBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+}
