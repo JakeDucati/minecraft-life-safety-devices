427c2ce67c5401e6c1f01474b10a12ed395a8cc9
Jake Ducati <107895867+JakeDucati@users.noreply.github.com>
2024-06-29 09:37:10 -0700

    Bug fixes
    - Fixed missing texture for BG-12 facing north
    - Fixed voxel shapes of BG-12
    - Fixed BG-12 "activated=true" not showing the activated model

--- src/main/java/com/lifesafetydevices/block/BG12.java
+++ src/main/java/com/lifesafetydevices/block/BG12.java
@@ -13,7 +13,6 @@ import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 public class BG12 extends HorizontalFacingBlock {
@@ -21,33 +20,11 @@ public class BG12 extends HorizontalFacingBlock {
     public static final BooleanProperty ACTIVATED = BooleanProperty.of("activated");
 
     // Voxel shapes for different orientations
-    private static final VoxelShape SHAPE_NORTH = VoxelShapes.union(
-        Block.createCuboidShape(0, 0, 0, 16, 3, 16),
-        Block.createCuboidShape(1, 3, 1, 15, 4, 15),
-        Block.createCuboidShape(2, 4, 2, 14, 5, 14),
-        Block.createCuboidShape(3, 5, 3, 13, 16, 13)
-    );
+    private static final VoxelShape SHAPE_NORTH = Block.createCuboidShape(3, 2, 13, 13, 14, 16);
+    private static final VoxelShape SHAPE_EAST = Block.createCuboidShape(0, 2, 3, 3, 14, 13);
+    private static final VoxelShape SHAPE_SOUTH = Block.createCuboidShape(3, 2, 0, 13, 14, 3);
+    private static final VoxelShape SHAPE_WEST = Block.createCuboidShape(13, 2, 3, 16, 14, 13);
 
-    private static final VoxelShape SHAPE_EAST = VoxelShapes.union(
-        Block.createCuboidShape(0, 0, 0, 16, 3, 16),
-        Block.createCuboidShape(1, 3, 1, 15, 4, 15),
-        Block.createCuboidShape(2, 4, 2, 14, 5, 14),
-        Block.createCuboidShape(3, 5, 3, 13, 16, 13)
-    );
-
-    private static final VoxelShape SHAPE_SOUTH = VoxelShapes.union(
-        Block.createCuboidShape(0, 0, 0, 16, 3, 16),
-        Block.createCuboidShape(1, 3, 1, 15, 4, 15),
-        Block.createCuboidShape(2, 4, 2, 14, 5, 14),
-        Block.createCuboidShape(3, 5, 3, 13, 16, 13)
-    );
-
-    private static final VoxelShape SHAPE_WEST = VoxelShapes.union(
-        Block.createCuboidShape(0, 0, 0, 16, 3, 16),
-        Block.createCuboidShape(1, 3, 1, 15, 4, 15),
-        Block.createCuboidShape(2, 4, 2, 14, 5, 14),
-        Block.createCuboidShape(3, 5, 3, 13, 16, 13)
-    );
 
     public BG12(Settings settings) {
         super(settings);
