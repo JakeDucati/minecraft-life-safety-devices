30395adb2ea130655506dfd50ea2239dc4ed58c7
Jake Ducati <107895867+JakeDucati@users.noreply.github.com>
2025-01-07 10:03:29 -0800

    added small_horn_strobe

--- src/main/java/com/lifesafetydevices/LifeSafetyDevices.java
+++ src/main/java/com/lifesafetydevices/LifeSafetyDevices.java
@@ -22,54 +22,62 @@ import com.lifesafetydevices.block.*;
 import com.lifesafetydevices.item.*;
 
 public class LifeSafetyDevices implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("life-safety-devices");
+        public static final Logger LOGGER = LoggerFactory.getLogger("life-safety-devices");
 
-    // item group
-    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
-            new Identifier("lifesafetydevices", "general"),
-            () -> new ItemStack(com.lifesafetydevices.LifeSafetyDevices.BG_12_ITEM));
+        // item group
+        public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+                        new Identifier("lifesafetydevices", "general"),
+                        () -> new ItemStack(com.lifesafetydevices.LifeSafetyDevices.BG_12_ITEM));
 
-    // blocks
-    public static final Block WHITE_EXIT_SIGN = new WhiteExitSign(
-            FabricBlockSettings.of(Material.METAL).strength(4.0f));
-    public static final Block BG_12 = new BG12(FabricBlockSettings.of(Material.METAL).strength(4.0f));
-    public static final Block TEST = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
-    public static final Block FIRE_BELL = new FireBell(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+        // blocks
+        public static final Block WHITE_EXIT_SIGN = new WhiteExitSign(
+                        FabricBlockSettings.of(Material.METAL).strength(4.0f));
+        public static final Block BG_12 = new BG12(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+        public static final Block TEST = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+        public static final Block FIRE_BELL = new FireBell(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+        public static final Block SMALL_HORN_STROBE = new SmallHornStrobe(
+                        FabricBlockSettings.of(Material.METAL).strength(4.0f));
 
-    // items
-    public static final Item KEY_ITEM = new KeyItem(new Item.Settings().group(ITEM_GROUP));
-    public static final Item BG_12_ITEM = new BG12Item(BG_12, new FabricItemSettings().group(ITEM_GROUP));
-    public static final Item WHITE_EXIT_SIGN_ITEM = new WhiteExitSignItem(WHITE_EXIT_SIGN,
-            new FabricItemSettings().group(ITEM_GROUP));
-    public static final Item FIRE_BELL_ITEM = new FireBellItem(FIRE_BELL,
-            new FabricItemSettings().group(ITEM_GROUP));
+        // items
+        public static final Item KEY_ITEM = new KeyItem(new Item.Settings().group(ITEM_GROUP));
+        public static final Item BG_12_ITEM = new BG12Item(BG_12, new FabricItemSettings().group(ITEM_GROUP));
+        public static final Item WHITE_EXIT_SIGN_ITEM = new WhiteExitSignItem(WHITE_EXIT_SIGN,
+                        new FabricItemSettings().group(ITEM_GROUP));
+        public static final Item FIRE_BELL_ITEM = new FireBellItem(FIRE_BELL,
+                        new FabricItemSettings().group(ITEM_GROUP));
+        public static final Item SMALL_HORN_STROB_ITEM = new SmallHornStrobeItem(SMALL_HORN_STROBE,
+                        new FabricItemSettings().group(ITEM_GROUP));
 
-    @Override
-    public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
+        @Override
+        public void onInitialize() {
+                LOGGER.info("Hello Fabric world!");
 
-        // sounds
-        ModSounds.registerSounds();
+                // sounds
+                ModSounds.registerSounds();
 
-        // key
-        Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "key"), KEY_ITEM);
+                // key
+                Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "key"), KEY_ITEM);
 
-        // white exit sign
-        Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "white_exit_sign"),
-                WHITE_EXIT_SIGN);
-        Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "white_exit_sign"),
-                WHITE_EXIT_SIGN_ITEM);
+                // white exit sign
+                Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "white_exit_sign"),
+                                WHITE_EXIT_SIGN);
+                Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "white_exit_sign"),
+                                WHITE_EXIT_SIGN_ITEM);
 
-        // bg-12 pull station
-        Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "bg-12"), BG_12);
-        Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "bg-12"), BG_12_ITEM);
+                // bg-12 pull station
+                Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "bg-12"), BG_12);
+                Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "bg-12"), BG_12_ITEM);
 
-        // test
-        Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "test"), TEST);
-        Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "test"),
-                new BlockItem(TEST, new FabricItemSettings().group(ITEM_GROUP)));
+                // test
+                Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "test"), TEST);
+                Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "test"),
+                                new BlockItem(TEST, new FabricItemSettings().group(ITEM_GROUP)));
 
-        Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "fire_bell"), FIRE_BELL);
-        Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "fire_bell"), FIRE_BELL_ITEM);
-    }
+                Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "fire_bell"), FIRE_BELL);
+                Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "fire_bell"), FIRE_BELL_ITEM);
+
+                // small horn strobe
+                Registry.register(Registry.BLOCK, new Identifier("lifesafetydevices", "small_horn_strobe"), SMALL_HORN_STROBE);
+                Registry.register(Registry.ITEM, new Identifier("lifesafetydevices", "small_horn_strobe"), SMALL_HORN_STROB_ITEM);
+        }
 }
---
+++ src/main/java/com/lifesafetydevices/block/SmallHornStrobe.java
@@ -0,0 +1,100 @@
+package com.lifesafetydevices.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.state.property.Properties;
+
+public class SmallHornStrobe extends HorizontalFacingBlock implements Waterloggable {
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    public static final BooleanProperty ACTIVATED = BooleanProperty.of("activated");
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+
+    // Voxel shapes for different orientations
+    private static final VoxelShape SHAPE_NORTH = Block.createCuboidShape(3, 2, 13, 13, 14, 16);
+    private static final VoxelShape SHAPE_EAST = Block.createCuboidShape(0, 2, 3, 3, 14, 13);
+    private static final VoxelShape SHAPE_SOUTH = Block.createCuboidShape(3, 2, 0, 13, 14, 3);
+    private static final VoxelShape SHAPE_WEST = Block.createCuboidShape(13, 2, 3, 16, 14, 13);
+
+    public SmallHornStrobe(Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState()
+                .with(FACING, Direction.NORTH)
+                .with(ACTIVATED, false)
+                // waterlog
+                .with(Properties.HORIZONTAL_FACING, Direction.NORTH)
+                .with(WATERLOGGED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING, ACTIVATED, WATERLOGGED);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState()
+                .with(FACING, ctx.getPlayerFacing().getOpposite())
+                // waterlog
+                .with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite())
+                .with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER));
+    }
+
+    // waterlog
+    @SuppressWarnings("deprecation")
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    // waterlog
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
+            WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return switch (state.get(FACING)) {
+            case EAST -> SHAPE_EAST;
+            case SOUTH -> SHAPE_SOUTH;
+            case WEST -> SHAPE_WEST;
+            default -> SHAPE_NORTH;
+        };
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return getOutlineShape(state, world, pos, context);
+    }
+}
